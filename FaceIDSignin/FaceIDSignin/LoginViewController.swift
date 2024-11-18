7c8d21d33f015318adc3cfa5b02a9cf1fb74b820
@Raman Mann <rjmann43@gmail.com>
2024-11-18 17:08:56 +0530

    improvements

--- FaceIDSignin/FaceIDSignin/LoginViewController.swift
+++ FaceIDSignin/FaceIDSignin/LoginViewController.swift
@@ -14,6 +14,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //MARK:- Config function
         configBioMetric()
        
     }
